3d8f23719fadb9f877c5b7978ae31a2f474d2d8e
yoemigpt <youssouf.emine@polymtl.ca>
2024-05-01 19:36:20 -0400

    fix replace warning.

--- fipe/encoding.py
+++ fipe/encoding.py
@@ -118,11 +118,15 @@ class FeatureEncoder:
         for c in self.columns:
             if self.data[c].nunique() == 2:
                 self.features[c] = FeatureType.BINARY
-                values = self.data[c].unique()
-                # TODO: do not use replace! Use map instead.
-                self.data.replace({
-                    c: {values[0]: 0, values[1]: 1}
-                }, inplace=True)
+                if self.data[c].dtype == "category":
+                    df = pd.get_dummies(self.data[c], drop_first=True)
+                    self.data.drop(columns=c, inplace=True)
+                    self.data[c] = df.iloc[:, 0]
+                else:
+                    values = self.data[c].unique()
+                    self.data.replace({
+                        c: {values[0]: 0, values[1]: 1}
+                    }, inplace=True)
 
     def fit_numerical_features(self):
         # For each column in the data
