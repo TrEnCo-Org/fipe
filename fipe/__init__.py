0e77373da452125aff1727835c65c665a6c6f35e
yoemigpt <youssouf.emine@polymtl.ca>
2024-05-01 16:43:04 -0400

    fix export types.

--- fipe/__init__.py
+++ fipe/__init__.py
@@ -1,13 +1,18 @@
+from .encoding import Encoder
 from .pruning import FIPEPruner
+
+from .typing import *
 from ._predict import (
     predict_single_proba,
     predict_proba,
     predict
 )
 
+
 __all__ = [
     'FIPEPruner',
+    'Encoder',
     'predict_single_proba',
     'predict_proba',
-    'predict'
+    'predict',
 ]
